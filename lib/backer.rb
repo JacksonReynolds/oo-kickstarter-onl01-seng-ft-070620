4cfb7b88d710983a7e113964382fe02c84ace67d
Jackson Reynolds <jwreynold@gmail.com>
2020-07-21 00:50:33 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+require 'project.rb'
+
+class Backer
+
+  attr_accessor :backed_projects
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end #initialize
+
+end #Backer
